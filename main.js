6a14410790afc06f0d6c6e3d920bae419138993e
xXFireFlowerXx <83987136+xXFireFlowerXx@users.noreply.github.com>
2021-09-16 17:24:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+var canvas = new fabric.Canvas("myCanvas")
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var player_object= "";
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img){
+        player_object = Img;
+        player_object.scaleToWidth(block_image_width)
+        player_object.scaleToHeight(block_image_height);
+        player_object.set({
+            top:block_y,
+            left:block_x
+        });
+        canvas.add(player_object)
+});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82')
+	{
+		new_image('rr1.png')
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		new_image('gr.png')
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		new_image('yr.png')
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		new_image('pr.png')
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	new_image('br.png')
+	}
+	
+}
+
